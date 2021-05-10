db1f00a81b8d6f0f85e7d6bc62a90db7cf77bc94
samuel-kmdi <79562829+samuel-kmdi@users.noreply.github.com>
2021-05-10 17:03:30 +0800

    Update Explosion2UnitTest.cs
    Working ChkVar_SinglePanelFixWindow

--- ModelLayer.Tests/Explosion2UnitTest.cs
+++ ModelLayer.Tests/Explosion2UnitTest.cs
@@ -1,21 +1,92 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelLayer.Model.Quotation;
+using ModelLayer.Model.Quotation.Frame;
+using ModelLayer.Model.Quotation.Panel;
+using ModelLayer.Model.Quotation.WinDoor;
+using ServiceLayer.CommonServices;
+using ServiceLayer.Services.FrameServices;
+using ServiceLayer.Services.PanelServices;
+using ServiceLayer.Services.QuotationServices;
+using ServiceLayer.Services.WindoorServices;
+using System.Windows.Forms;
+using static ModelLayer.Model.Quotation.QuotationModel;
 
 namespace ModelLayer.Tests
 {
     [TestClass]
     public class Explosion2UnitTest
     {
+        IQuotationModel _qouteModel;
+        IWindoorServices _windoorServices;
+        IFrameServices _frameServices;
+        IPanelServices _panelServices;
+
 
         [TestInitialize]
         public void SetUp()
         {
+            IQuotationServices _quotationServices = new QuotationServices(new ModelDataAnnotationCheck());
+            _qouteModel = _quotationServices.AddQuotationModel("Sample123");
 
+            _windoorServices = new WindoorServices(new ModelDataAnnotationCheck());
+            _frameServices = new FrameServices(new ModelDataAnnotationCheck());
+            _panelServices = new PanelServices(new ModelDataAnnotationCheck());
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void ChkVar_SinglePanelFixWindow()
         {
+            int total_wd = 1000, total_height = 2000;
+
+            IWindoorModel _windoorModel = _windoorServices.AddWindoorModel(total_wd, total_height, "C70", 1);
+            _qouteModel.Lst_Windoor.Add(_windoorModel);
+
+            IFrameModel _frameModel = _frameServices.AddFrameModel(total_wd,
+                                                                   total_height,
+                                                                   FrameModel.Frame_Padding.Window,
+                                                                   1.0f,
+                                                                   1.0f,
+                                                                   FrameProfile_ArticleNo._7502,
+                                                                   1);
+            _windoorModel.lst_frame.Add(_frameModel);
+
+            int wd = _frameModel.Frame_Width - (int)(_frameModel.Frame_Type - 10) * 2,
+                ht = _frameModel.Frame_Height - (int)(_frameModel.Frame_Type - 10) * 2;
+
+            IPanelModel _panelModel = _panelServices.AddPanelModel(wd,
+                                                                   ht,
+                                                                   new Control(),
+                                                                   new UserControl(),
+                                                                   new UserControl(),
+                                                                   new UserControl(),
+                                                                   "Fixed Panel",
+                                                                   true,
+                                                                   1.0f,
+                                                                   _frameModel,
+                                                                   null,
+                                                                   "6-8mm",
+                                                                   GlazingBead_ArticleNo._2452,
+                                                                   1);
+            _frameModel.Lst_Panel.Add(_panelModel);
+
+            _qouteModel.GetListOfMaterials();
+
+            Assert.AreEqual(FrameProfile_ArticleNo._7502, _frameModel.Frame_ArtNo);
+            Assert.AreEqual(1005, _frameModel.Frame_ExplosionWidth);
+            Assert.AreEqual(2005, _frameModel.Frame_ExplosionHeight);
+            Assert.AreEqual(FrameReinf_ArticleNo._R676, _frameModel.Frame_ReinfArtNo);
+            Assert.AreEqual(932, _frameModel.Frame_ReinfWidth);
+            Assert.AreEqual(1932, _frameModel.Frame_ReinfHeight);
+            Assert.AreEqual(1, _frameModel.Frame_PUFoamingQty);
+            Assert.AreEqual(2, _frameModel.Frame_SealantWHQty);
+
+            Assert.AreEqual(GlazingBead_ArticleNo._2452, _panelModel.PanelGlazingBead_ArtNo);
+            Assert.AreEqual(934, _panelModel.Panel_GlazingBeadWidth);
+            Assert.AreEqual(1934, _panelModel.Panel_GlazingBeadHeight);
+            Assert.AreEqual(928, _panelModel.Panel_GlassWidth);
+            Assert.AreEqual(1928, _panelModel.Panel_GlassHeight);
+            Assert.AreEqual(1, _panelModel.Panel_GlazingSpacerQty);
+            Assert.AreEqual(1, _panelModel.Panel_SealantWHQty);
         }
     }
 }
