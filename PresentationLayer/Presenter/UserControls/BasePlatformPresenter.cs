2200fafabf884df0360dcfa4052967848e6d9503
Ej1224 <39943828+Ej1224@users.noreply.github.com>
2021-06-10 18:31:19 +0800

    5PM

--- PresentationLayer/Presenter/UserControls/BasePlatformPresenter.cs
+++ PresentationLayer/Presenter/UserControls/BasePlatformPresenter.cs
@@ -269,6 +269,133 @@ namespace PresentationLayer.Presenter.UserControls
             return lst_ht;
         }
 
+
+        public List<int> WidthList_ToPaint(int flpMain_width, int[,] arr_wd_locX)
+        {
+            List<int> Width_List = new List<int>();
+
+            int[] arr_wd = new int[arr_wd_locX.GetLength(0)];
+            int[] arr_locX = new int[arr_wd_locX.GetLength(0)];
+
+            for (int i = 0; i < arr_wd_locX.GetLength(0); i++)
+            {
+                arr_wd[i] = arr_wd_locX[i, 0];
+            }
+
+            for (int i = 0; i < arr_wd_locX.GetLength(0) ; i++)
+            {
+                arr_locX[i] = arr_wd_locX[i, 1];
+            }
+
+            Array.Sort(arr_locX, arr_wd);
+
+            List<int> lst_wd = new List<int>();
+            List<int> lst_of_inserted_locX = new List<int>();
+            List<int> lst_of_inserted_locX_indices = new List<int>();
+
+            for (int i = 0; i < arr_locX.Length; i++)
+            {
+                int curr_locX = arr_locX[i],
+                    curr_wd = arr_wd[i];
+                if (lst_of_inserted_locX.Contains(curr_locX) == false)
+                {
+                    lst_wd.Add(curr_wd);
+                    lst_of_inserted_locX.Add(curr_locX);
+                    lst_of_inserted_locX_indices.Add(i);
+                }
+                else if (lst_of_inserted_locX.Contains(curr_locX) == true)
+                {
+                    int indx_of_locX = lst_of_inserted_locX.IndexOf(curr_locX),
+                        indx_of_locXIndex = lst_of_inserted_locX_indices[indx_of_locX],
+                        wd_to_compare = arr_wd[indx_of_locXIndex];
+                    if (curr_wd < wd_to_compare)
+                    {
+                        lst_wd[indx_of_locX] = curr_wd;
+                    }
+                }
+            }
+
+            int total_wd = 0,
+                curr_lst_wd = 0;
+            for (int i = 0; i < lst_wd.Count; i++)
+            {
+                curr_lst_wd = lst_wd[i];
+                total_wd += curr_lst_wd;
+                if (total_wd <= flpMain_width)
+                {
+                    Width_List.Add(curr_lst_wd);
+                }
+                else if (total_wd > flpMain_width)
+                {
+                    total_wd -= curr_lst_wd;
+                }
+            }
+
+            return Width_List;
+        }
+
+        public List<int> HeightList_ToPaint(int flpMain_height, int[,] arr_ht_locY)
+        {
+            List<int> Height_List = new List<int>();
+
+            int[] arr_ht = new int[arr_ht_locY.GetLength(0)];
+            int[] arr_locY = new int[arr_ht_locY.GetLength(0)];
+
+            for (int i = 0; i < arr_ht_locY.GetLength(0); i++)
+            {
+                arr_ht[i] = arr_ht_locY[i, 0];
+            }
+
+            for (int i = 0; i < arr_ht_locY.GetLength(0); i++)
+            {
+                arr_locY[i] = arr_ht_locY[i, 1];
+            }
+
+            Array.Sort(arr_locY, arr_ht);
+
+            List<int> lst_ht = new List<int>();
+            List<int> lst_of_inserted_locY = new List<int>();
+            List<int> lst_of_inserted_locY_indices = new List<int>();
+
+            for (int i = 0; i < arr_locY.Length; i++)
+            {
+                int curr_locY = arr_locY[i],
+                    curr_ht = arr_ht[i];
+                if (lst_of_inserted_locY.Contains(curr_locY) == false)
+                {
+                    lst_ht.Add(curr_ht);
+                    lst_of_inserted_locY.Add(curr_locY);
+                    lst_of_inserted_locY_indices.Add(i);
+                }
+                else if (lst_of_inserted_locY.Contains(curr_locY) == true)
+                {
+                    int indx_of_locY = lst_of_inserted_locY.IndexOf(curr_locY),
+                        indx_of_locYIndex = lst_of_inserted_locY_indices[indx_of_locY],
+                        ht_to_compare = arr_ht[indx_of_locYIndex];
+                    if (curr_ht < ht_to_compare)
+                    {
+                        lst_ht[indx_of_locY] = curr_ht;
+                    }
+                }
+            }
+
+            int total_ht = 0;
+            for (int i = 0; i < lst_ht.Count; i++)
+            {
+                total_ht += lst_ht[i];
+                if (total_ht <= flpMain_height)
+                {
+                    Height_List.Add(lst_ht[i]);
+                }
+                else if (total_ht > flpMain_height)
+                {
+                    total_ht -= lst_ht[i];
+                }
+            }
+
+            return Height_List;
+        }
+
         public void Invalidate_flpMain()
         {
             _flpMain.Invalidate();
