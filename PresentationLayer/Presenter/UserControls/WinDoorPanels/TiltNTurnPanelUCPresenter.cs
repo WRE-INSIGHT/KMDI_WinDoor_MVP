fba7672a85a6e6943bfa584410b5d461e0fb5b06
samuel-kmdi <79562829+samuel-kmdi@users.noreply.github.com>
2023-05-29 08:48:39 +0800

    baseflatform mesh

--- PresentationLayer/Presenter/UserControls/WinDoorPanels/TiltNTurnPanelUCPresenter.cs
+++ PresentationLayer/Presenter/UserControls/WinDoorPanels/TiltNTurnPanelUCPresenter.cs
@@ -330,7 +330,9 @@ namespace PresentationLayer.Presenter.UserControls.WinDoorPanels
 
             int font_size = 30,
                 outer_line = 10,
-                inner_line = 15;
+                inner_line = 15,
+                rectThickness = 1;
+
 
             int ndx_zoomPercentage = Array.IndexOf(_mainPresenter.windoorModel_MainPresenter.Arr_ZoomPercentage, _frameModel.Frame_Zoom);
 
@@ -343,6 +345,8 @@ namespace PresentationLayer.Presenter.UserControls.WinDoorPanels
                 font_size = 15;
                 outer_line = 5;
                 inner_line = 8;
+                rectThickness = 2;
+
             }
             else if (ndx_zoomPercentage == 1)
             {
@@ -436,6 +440,33 @@ namespace PresentationLayer.Presenter.UserControls.WinDoorPanels
 
             #endregion
 
+            #region Mesh
+            if (_panelModel.Panel_GlassThicknessDesc.Contains("Mesh"))
+            {
+                int cond = tiltNTurn.ClientRectangle.Width + tiltNTurn.ClientRectangle.Height;
+
+                int maxWidth = tiltNTurn.ClientRectangle.Width;
+
+                for (int i = 10; i < cond; i += 10)
+                {
+                    g.DrawLine(Pens.LightSlateGray, new Point(0, i), new Point(i, 0));
+
+                }
+
+                for (int i = 10; i < cond; i += 10)
+                {
+                    g.DrawLine(Pens.LightSlateGray, new Point(maxWidth - i, 0), new Point(tiltNTurn.ClientRectangle.Width, i));
+
+                }
+               
+                    g.DrawRectangle(new Pen(Color.DarkGray, 15 / rectThickness), new Rectangle(8 / rectThickness,
+                                                                           8 / rectThickness,
+                                                                           tiltNTurn.ClientRectangle.Width - 17 / rectThickness,
+                                                                           tiltNTurn.ClientRectangle.Height - 17 / rectThickness));
+
+                
+            }
+            #endregion
 
             string glassType = "";
             if (_panelModel.Panel_GlassThicknessDesc != null)
