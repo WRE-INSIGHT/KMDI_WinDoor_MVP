c586e30682dbb46c80b4b102d67bf2ca0ffe9468
JB Alejandrino <118698411+jv-kmdi@users.noreply.github.com>
2023-12-05 11:17:10 +0800

    Load_update

--- PresentationLayer/Presenter/MainPresenter.cs
+++ PresentationLayer/Presenter/MainPresenter.cs
@@ -11394,22 +11394,8 @@ namespace PresentationLayer.Presenter
                                  false);
                 //clear
 
-                //foreach (Control ctrl in _pnlMain.Controls.OfType<Control>().ToList())
-                //{
-                //    ctrl.Dispose();
-                //}
-                //foreach (Control ctrl in _pnlPropertiesBody.Controls.OfType<Control>().ToList())
-                //{
-                //    ctrl.Dispose();
-                //}
-
                 _pnlMain.Controls.Clear();
                 _pnlPropertiesBody.Controls.Clear();
-
-                //foreach (Control ctrl in _pnlMain.Controls.OfType<Control>().ToList())
-                //{
-                //    ctrl.Dispose();
-                //}
 
                 _frmDimensionPresenter.SetValues(_windoorModel.WD_width, _windoorModel.WD_height);
 
