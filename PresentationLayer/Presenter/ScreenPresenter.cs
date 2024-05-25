024ff620acd5bd18d775d81ead353caac6d9e37d
JB Alejandrino <118698411+jv-kmdi@users.noreply.github.com>
2024-05-25 08:14:48 +0800

    Trim Item Desc

--- PresentationLayer/Presenter/ScreenPresenter.cs
+++ PresentationLayer/Presenter/ScreenPresenter.cs
@@ -246,7 +246,9 @@ namespace PresentationLayer.Presenter
                                                 setDesc = " ";
                                             }
 
-                                            Spap.Screen_Description = item.Screen_Description + setDesc;
+
+
+                                            Spap.Screen_Description = item.Screen_Description.Trim()+ setDesc;
                                             Spap.Screen_Set = item.Screen_Set;
                                             Spap.Screen_DisplayedDimension = item.Screen_DisplayedDimension;
                                             Spap.Screen_UnitPrice = item.Screen_UnitPrice;
